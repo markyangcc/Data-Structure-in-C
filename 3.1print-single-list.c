71b47171dcb51218bc2d88179118b7804830aa4c
markyangcc <guangshengyang01@gmail.com>
2020-07-26 16:07:38 +0800

    create 3.1print-single-list.c

---
+++ 3.1print-single-list.c
@@ -0,0 +1,61 @@
+/*编写一个打印单链表的所有元素的程序*/
+
+//采用带头结点的单链表实现，只有头结点是全局变量，其余变量通过函数进行参数传递，尽量避免使用全局变量
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node header; //declare a global node called header
+
+void createlist(struct node *header, struct node **ptr, int data);
+void printlist(struct node *L);
+
+int main(void)
+{
+    // initialize the header
+    struct node *header = malloc(sizeof(struct node));
+    if (!header)
+    {
+        perror("malloc node");
+        exit(EXIT_FAILURE);
+    };
+    header->next = NULL;
+
+    struct node *ptr = header; //ptr is used to move on the list
+
+    createlist(header, &ptr, 1);
+    createlist(header, &ptr, 2);
+
+    printlist(header->next); //header->next is the beginning of the actually linked list, except the header
+
+    return 0;
+}
+
+void createlist(struct node *header, struct node **ptr, int data)
+{
+    struct node *newnode = malloc(sizeof(struct node));
+    //make the list link to headerer
+    if (header->next == NULL)
+        header->next = newnode;
+    //link new node to previous list
+    (*ptr)->next = newnode;
+    (*ptr) = newnode;
+
+    newnode->data = data;
+    newnode->next = NULL;
+}
+
+void printlist(struct node *L)
+{
+    while (L != NULL)
+    {
+        printf("%d\t", L->data);
+        L = L->next;
+    }
+}
--- code.c
+++ code.c
@@ -1,59 +1,13 @@
-/*编写一个打印单链表的所有元素的程序*/
+/*给你一个链表L和另一个链表P，他们包含以升序排列的整数。操作PrintLots(L,P)将打印L中那些由P指定的位置上的元素。
+例如，如果P=1，3，4，6，那么L中的第1，3，4，6个元素将被打印出来
+写出程序PrintLots(L,P)。你应该只使用基本的表操作。该程序的运行时间是多少？
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
-    int data;
-    struct node *next;
-};
-
-struct node header; //declare a global node called header
-
-void createlist(struct node *header, struct node **ptr, int data);
-void printlist(struct node *L);
-
 int main(void)
 {
-    // initialize the header
-    struct node *header = malloc(sizeof(struct node));
-    if (!header)
-    {
-        perror("malloc node");
-        exit(EXIT_FAILURE);
-    };
-    header->next = NULL;
-
-    struct node *ptr = header; //ptr is used to move on the list
-
-    createlist(header, &ptr, 1);
-    createlist(header, &ptr, 2);
-
-    printlist(header->next); //header->next is the beginning of the actually linked list, except the header
 
     return 0;
-}
-
-void createlist(struct node *header, struct node **ptr, int data)
-{
-    struct node *newnode = malloc(sizeof(struct node));
-    //make the list link to headerer
-    if (header->next == NULL)
-        header->next = newnode;
-    //link new node to previous list
-    (*ptr)->next = newnode;
-    (*ptr) = newnode;
-
-    newnode->data = data;
-    newnode->next = NULL;
-}
-
-void printlist(struct node *L)
-{
-    while (L != NULL)
-    {
-        printf("%d\t", L->data);
-        L = L->next;
-    }
 }
